96bb8f92635e117a6a7eb3f623b2b88e97626ac3
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-11-13 16:32:18 -0500

    roulette gun step by step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import RouletteGun from './drill1/RouletteGun'
+import RouletteGun2 from './drill1/RouletteGun2'
 
 function App() {
   return (
     <div className="App">
-      <RouletteGun />
+      <RouletteGun2 />
     </div>
   );
 }
