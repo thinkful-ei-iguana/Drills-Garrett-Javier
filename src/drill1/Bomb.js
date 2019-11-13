daba8e876043220ee75a9ade7924dc14117176ae
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-11-13 14:27:14 -0500

    completed tick tock boom. 1 2 boomgit add .

--- src/drill1/Bomb.js
+++ src/drill1/Bomb.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 
 
 class Bomb extends React.Component{
-    state = {
-
+    constructor(props){
+        super(props)
+        this.state = {
+            datetime: new Date(),
+        }
+        //console.log('constructor')
     }
 
+    componentDidMount(){
+        //console.log('componentDidMount')
+        this.interval = setInterval(() => {
+            //console.log('setInterval')
+            this.setState({
+
+                datetime: new Date().getSeconds(),
+            });
+        }, 1000);
+    }
+    handleChange = () => {
+        if(this.state.datetime % 3 === 0){
+            console.log('boom')
+            return 'BOOM!!!'
+        } else if(this.state.datetime % 2 === 1){
+            console.log('tick')
+            return 'tick'
+        } else if(this.state.datetime % 2 === 0){
+                console.log('tock')
+                return 'tock'
+            }
+        }
+    
+    
+
+
+    componentWillUnmount(){
+        clearInterval(this.interval) 
+    }
+
+
 render(){
+    console.log(this.state.datetime)
     return(
-        <p>hello</p>
+        <div onChange={this.handleChange}><span>{this.handleChange()}</span></div>
     );
 }
 
